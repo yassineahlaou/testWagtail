87c82a82e7a02d50b00c16f037c6fbccde753c01
yassineahlaou <yassineahlaou@gmail.com>
2021-08-05 17:56:59 +0100

    bog with Wagtail
    bog with Wagtail

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-0&othi4jky3b&4_z^q!8d@@63b(p1l9z35i(pfe@1qh)h=@7cb'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
